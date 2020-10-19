23a273f101876a4cee94bf0a2e606e4b3183c706
Grigor <grigor.zyapkov@gmail.com>
2020-10-19 19:02:02 +0300

    refine MinimumCoinCountLimited

--- src/main/java/com/zyapkov/algorithms/dynamicprogramming/MinimumCoinCountLimited.java
+++ src/main/java/com/zyapkov/algorithms/dynamicprogramming/MinimumCoinCountLimited.java
@@ -7,30 +7,22 @@ package com.zyapkov.algorithms.dynamicprogramming;
 
 public class MinimumCoinCountLimited {
 
-  public static int minCoinsLimited(int value, int[] coins, int[] limits, int size) {
-    int k = getTotalCoinsCount(limits, size);
-    int[] container = new int[k];
-    int index = 0;
-    for (int i = 0; i < size; i++) {
-      for (int j = 0; j < limits[i]; j++) {
-        container[index++] = coins[i];
-      }
-    }
+  public static int minCoinsLimited(int value, int[][] coins, int size) {
+    int k = getTotalCoinsCount(coins, size);
+    int[] container = createAllCoinsList(coins, size, k);
 
-    int[][] matrix = new int[k + 1][value + 1];
-    for (int i = 0; i < k + 1; i++) {
-      matrix[i][0] = 0;
-    }
-    for (int j = 1; j < value + 1; j++) {
-      matrix[0][j] = Integer.MAX_VALUE;
-    }
+    int rows = k + 1;
+    int cols = value + 1;
+    int[][] matrix = initMatrix(rows, cols);
 
-    for (int i = 1; i < k + 1; i++) {
-      for (int j = 1; j < value + 1; j++) {
+    for (int i = 1; i < rows; i++) {
+      for (int j = 1; j < cols; j++) {
 
         matrix[i][j] = matrix[i - 1][j];
-        if (container[i - 1] <= j && Integer.MAX_VALUE != matrix[i - 1][j - container[i - 1]]) {
-          matrix[i][j] = Math.min(matrix[i - 1][j], 1 + matrix[i - 1][j - container[i - 1]]);
+        if (container[i - 1] <= j && matrix[i - 1][j - container[i - 1]] != -1) {
+          matrix[i][j] = matrix[i][j] != -1 ?
+              Math.min(matrix[i - 1][j], 1 + matrix[i - 1][j - container[i - 1]]) :
+              1 + matrix[i - 1][j - container[i - 1]];
         }
       }
     }
@@ -38,10 +30,34 @@ public class MinimumCoinCountLimited {
     return matrix[k][value];
   }
 
-  private static int getTotalCoinsCount(int[] limits, int size) {
+  private static int[][] initMatrix(int rows, int cols) {
+    int[][] matrix = new int[rows][cols];
+    for (int i = 0; i < rows; i++) {
+      matrix[i][0] = 0;
+    }
+    for (int j = 1; j < cols; j++) {
+      matrix[0][j] = -1;
+    }
+
+    return matrix;
+  }
+
+  private static int[] createAllCoinsList(int[][] coins, int oldSize, int newSize) {
+    int[] list = new int[newSize];
+    int index = 0;
+    for (int i = 0; i < oldSize; i++) {
+      for (int j = 0; j < coins[1][i]; j++) {
+        list[index++] = coins[0][i];
+      }
+    }
+
+    return list;
+  }
+
+  private static int getTotalCoinsCount(int[][] limits, int size) {
     int k = 0;
     for (int i = 0; i < size; i++) {
-      k += limits[i];
+      k += limits[1][i];
     }
 
     return k;
@@ -49,8 +65,8 @@ public class MinimumCoinCountLimited {
 
   public static void main(String[] args) {
     int size = 4;
-    int[] coins = new int[]{1, 2, 3, 4};
-    int[] limits = new int[]{1, 1, 0, 1};
-    System.out.println("Minimum coins required are " + minCoinsLimited(10, coins, limits, size));
+    // 2-dimensional matrix representing coins(first row) and supply(second row)
+    int[][] coins = new int[][]{{1, 2, 3, 4},{1, 1, 0, 1}};
+    System.out.println("Minimum coins required are " + minCoinsLimited(10, coins, size));
   }
 }
